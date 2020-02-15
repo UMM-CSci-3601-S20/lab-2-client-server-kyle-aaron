4957c1505d141f1e99f935c44dc3a96a7fdb455c
Aaron Otten <otten099@morris.umn.edu>
2020-02-15 16:05:59 -0600

    FilterTodosByCombinedFIlters tests updated
    Added specific arbitrary testing for  combined filters

--- server/src/test/java/umm3601/todo/FilterTodosByCombinedFiltersFromDB.java
+++ server/src/test/java/umm3601/todo/FilterTodosByCombinedFiltersFromDB.java
@@ -31,8 +31,40 @@ public class FilterTodosByCombinedFiltersFromDB {
 
     queryParams.clear();
     queryParams.put("category", Arrays.asList(new String[] { "video games" }));
-    queryParams.put("status", Arrays.asList(new String[] { "false" }));
+    queryParams.put("status", Arrays.asList(new String[] { "incomplete" }));
     Todo[] falseVideoGamesTodos = db.listTodos(queryParams);
     assertEquals(41, falseVideoGamesTodos.length, "Incorrect number of todos with category video games and status false");
+  }
+
+  @Test
+  public void listTodosWithArbitraryFilters() throws IOException {
+    tDatabase db = new tDatabase("/todos.json");
+    Map<String, List<String>> queryParams = new HashMap<>();
+
+    queryParams.put("owner", Arrays.asList(new String[] { "Blanche" }));
+    queryParams.put("status", Arrays.asList(new String[] { "complete" }));
+    queryParams.put("limit", Arrays.asList(new String[] { "12" }));
+    queryParams.put("orderBy", Arrays.asList(new String[] { "category" }));
+    Todo[] arbitraryTodos = db.listTodos(queryParams);
+    int blancheCount = 0, trueCount = 0;
+    for (int i = 0; i < 12; i++) {
+      Todo currentTodo = arbitraryTodos[i];
+      if (currentTodo.owner.equals("Blanche") && currentTodo.status == true) {
+        blancheCount++;
+        trueCount++;
+      }
+    }
+    int precedingGroceryCount = 0;
+  
+    for (int i = 0; i < 3; i++) {
+      Todo currentTodo = arbitraryTodos[i];
+        if (currentTodo.category.equals("groceries")) {
+            precedingGroceryCount++;    
+        }
+    }
+    assertEquals(12, blancheCount, "Owners are not filtered correctly");
+    assertEquals(12, trueCount, "Status is not filtered correctly");
+    assertEquals(12, arbitraryTodos.length, "More than the expected number of todos appeared");
+    assertEquals(3, precedingGroceryCount, "A different number of grocery categories were found.");
   }
 }
