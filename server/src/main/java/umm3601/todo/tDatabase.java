ee030edb8dd16112e68e6e2311c9ddaae448946a
Aaron Otten <otten099@morris.umn.edu>
2020-02-15 14:36:43 -0600

    Removed unused import

--- server/src/main/java/umm3601/todo/tDatabase.java
+++ server/src/main/java/umm3601/todo/tDatabase.java
@@ -3,7 +3,6 @@ package umm3601.todo;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
